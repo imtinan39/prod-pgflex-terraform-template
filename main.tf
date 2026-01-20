c4075918b252b17fd0c4e579be71fe5cd439ef9a
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-01-20 10:26:02 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,88 @@
+/* ============ RESOURCE GROUP ============ */
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+/* ============ VNET ============ */
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = var.vnet_address_space
+  tags                = var.tags
+}
+
+/* ============ DELEGATED SUBNET ============ */
+resource "azurerm_subnet" "pgflex" {
+  name                 = var.pg_subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.pg_subnet_prefixes
+
+  delegation {
+    name = "pgflex-delegation"
+
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/action"
+      ]
+    }
+  }
+}
+
+/* ============ PRIVATE DNS ============ */
+resource "azurerm_private_dns_zone" "pg" {
+  name                = var.private_dns_zone_name
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "pg_link" {
+  name                  = "${var.vnet_name}-pgflex-dns-link"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.pg.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  registration_enabled  = false
+  tags                  = var.tags
+}
+
+/* ============ POSTGRES FLEXIBLE SERVER ============ */
+resource "azurerm_postgresql_flexible_server" "pg" {
+  name                = var.server_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  administrator_login    = var.administrator_login
+  administrator_password = var.administrator_password
+
+  version    = var.postgres_version
+  sku_name   = var.sku_name
+  storage_mb = var.storage_mb
+
+  backup_retention_days        = var.backup_retention_days
+  geo_redundant_backup_enabled = var.geo_redundant_backup_enabled
+  storage_auto_grow_enabled    = var.storage_auto_grow_enabled
+
+  zone = var.zone
+
+  public_network_access_enabled = false
+  delegated_subnet_id           = azurerm_subnet.pgflex.id
+  private_dns_zone_id           = azurerm_private_dns_zone.pg.id
+
+  tags = var.tags
+
+  depends_on = [
+    azurerm_private_dns_zone_virtual_network_link.pg_link
+  ]
+}
+
+/* ============ SERVER PARAMETERS ============ */
+resource "azurerm_postgresql_flexible_server_configuration" "params" {
+  for_each  = var.server_parameters
+  name      = each.key
+  server_id = azurerm_postgresql_flexible_server.pg.id
+  value     = each.value
+}
